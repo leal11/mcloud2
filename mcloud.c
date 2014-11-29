79864c80c6ee8b80749d0d1c31c7a30683de6c1c
ivanparada412 <ivan_parada412@hotmail.com>
2014-11-29 17:11:41 -0600

    Update mcloud.c

--- mcloud.c
+++ mcloud.c
@@ -7,8 +7,13 @@ los archivos, manejaríamos muchos.
 -Les voy a mostrar algunas variables globales que pueden usar en sus funciones, si necesitan más las declaran en la funcion
 -El archivo de texto que guarda la biblioteca musical se llama biblioteca.txt, 
 **/
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
 void registroa(int accion, int numerocatalogo, char fechare[]);
 void baja(void);
+void altausuario(void);
 void bitacora(char useraction[], char escuchada[]);
 int accesoadmin(char usuario[], char contrasena[]);
 int accesouser(char nickname[], char pass[]);
@@ -22,6 +27,13 @@ struct catalogo
   char cancion[5][20]; // arreglo de 5 canciones de 20 caracteres de largo
 } disco, lista[100];
 
+struct usuario
+{
+  char nombre[40];
+  char nickname[20];
+  char password[20];
+  char correo[30];
+} nuevousuario;
 char fecha[20];
 
 int main()
@@ -114,7 +126,7 @@ bitacora(char user[], char escuchada[])
   fclose(archivobitacora);
   
 }
-accesoadmin(char usuario[], char contrasena[])
+int accesoadmin(char usuario[], char contrasena[])
 {
   FILE* accesoa;
   char admincorrect[20];
@@ -122,6 +134,51 @@ accesoadmin(char usuario[], char contrasena[])
   accesoa = fopen("accesoadmin.dat", "rb");
   fscanf(accesoa, "%s", admincorrect);
   fscanf(accesoa, "%s", passwordcorrect);
+  if(strcmp(admincorrect, usuario) == 0 && strcmp(passwordcorrect, contrasena) == 0)
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+int accesouser(char nick[], char pass[])
+{
+  FILE* accesou;
+  int flag = 0;
+  int cont,  size;
+  accesou = fopen("accesousuarios.dat", "rb");
+ fseek(accesou, 0, SEEK_END);
+ size = ftell(accesou) / sizeof(nuevousuario);
+ fseek(accesou, 0, SEEK_SET);
+ for (cont = 0; cont < size; cont++)
+ {
+    fread(&nuevousuario, sizeof(nuevousuario), accesou);
+    if (strcmp(nuevousuario.nickname, nick) == 0 && strcmp(nuevousuario.password, pass) == 0)
+    {
+      flag = 1;
+    }
+ }
+  return flag;
   
+}
+void altausuario(void)
+{
+  FILE* adduser;
+  
+  printf("Cual es su nombre completo? :");
+  gets(nuevousuario.nombre);
+  printf("Cual es su nickname?(no mas de 20 caracteres):");
+  gets(nuevousuario.nickname);
+  printf("Cual es su contraseña?(no mas de 20 caracteres): ");
+  gets(nuevousuario.password);
+  printf("Cuál es su correo?: ");
+  gets(nuevousuario.correo);
+  
+  adduser = fopen("accesousuarios.dat", "ab");
+  fwrite(&nuevousuario, sizeof(nuevousuario), adduser);
+  fclose(adduser);
   
 }
